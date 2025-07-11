160b7d731108a1a6c5131ff231dca5ca8c1e0867
jumashafara <jumashafara0@gmail.com>
2025-07-11 10:47:42 +0300

    feat: added poetry

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,8 @@ CSRF_TRUSTED_ORIGINS = [
      "http://localhost:8000",  # Vite default port
     "http://localhost:8001",
     "http://localhost:8002",
-    "http://localhost:8003",    
+    "http://localhost:8003",
+    "https://easi.ac.ug"
 ]
 
 # Application definition
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
     'rest_framework_simplejwt',
+    'rest_framework_nested',
     # Local apps
     'accounts',
     'courses',
@@ -170,6 +172,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://localhost:3002",
     "http://localhost:3003",
+    "https://easi.ac.ug"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
